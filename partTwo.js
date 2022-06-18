d75497f20a17abea6783805b798790dee7317638
Nhat Hoang <johnhoanghtx@gmail.com>
2022-06-18 13:52:23 -0500

    DONE

--- partTwo.js
+++ partTwo.js
@@ -2,19 +2,25 @@ const perf = require('execution-time')();
 
 //SUM ZERO//
 
+//O(n^2)//
+
 const addToZero = (arr) => {
     
-    let result = false;
+  let result = false;
     
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[i] + arr[j] === 0) {
-          result = true;
-        } 
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length; j++) {
+
+      if (arr[i] + arr[j] === 0) {
+        result = true;
       }
+
     }
 
-    return result;
+  }
+
+  return result;
 
 }
 
@@ -32,15 +38,18 @@ console.log("TRUE : ", addToZero([1, 2, 3, -2]));
 
 //UNIQUE CHARACTERS//
 
+//O(n)//
+
 const hasUniqueChars = (str) => {
 
-    let uniqueStr = new Set([])
+  let uniqueStr = new Set([]);
+  let lowerCaseStr = str.toLowerCase();
 
-    for (let i = 0; i < str.length; i++) {
-      uniqueStr.add(str[i])
-    }
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    uniqueStr.add(lowerCaseStr[i])
+  }
 
-    return uniqueStr.size === str.length
+  return uniqueStr.size === lowerCaseStr.length;
 
 }
 
@@ -49,26 +58,55 @@ hasUniqueChars("Moonday"); //148.114 μs//
 
 console.log("TRUE : ", hasUniqueChars("Monday"));
 console.log("FALSE : ", hasUniqueChars("Moonday"));
+console.log("FALSE : ", hasUniqueChars("Bob"));
 
 //PANGRAM SENTENCE//
 
+//O(n)//
 
+const isPangram = (str) => {
+  
+  let newStr = str.toLowerCase().replace(/\s/g, '');
+  
+  let pangramSet = new Set([]);
+
+  for (let i = 0; i < newStr.length; i++) {
+    pangramSet.add(newStr[i])  
+  }
+
+  if(pangramSet.size >= 26) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+isPangram("The quick brown fox jumps over the lazy dog!"); //115.351 μs//
+isPangram("I like cats, but not mice"); //112.682 μs//
+
+console.log("TRUE",isPangram("The quick brown fox jumps over the lazy dog!"));
+console.log("FALSE", isPangram("I like cats, but not mice"));
+console.log("TRUE",isPangram("AaBbCcDdEeFfGgHhIiJjKkLlMmNnLlOoPpQqRrSsTtUuVvWwXxYyZz"));
+console.log("FALSE",isPangram(""));
 
 //LONGEST WORD//
 
+//O(n)//
+
 const findLongestWord = (arr) => {
 
-    let longestDefault = arr[0].length;
+  let longestDefault = arr[0].length;
   
-    for (let i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
 
-      if(arr[i].length > longestDefault) {
-        longestDefault = arr[i].length;
-      }
-
+    if(arr[i].length > longestDefault) {
+      longestDefault = arr[i].length;
     }
+
+  }
   
-    return longestDefault;
+  return longestDefault;
     
 }
 
@@ -77,8 +115,3 @@ findLongestWord(["hi", "hello"]) //105.406 μs//
 console.log("Should be 5 : ",findLongestWord(["hi","hello"]))
 console.log("Should be 12 : ",findLongestWord(["hi","hello", "hola", "YOYOYOYOYOYO"]))
 
-// perf.start();
-// findLongestWord(["hi", "hello"]);
-// let results = perf.stop();
-
-// console.log("findLongest", results.preciseWords);
